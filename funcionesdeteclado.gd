c2945f94cf4293f3f1133645b6c67b8a19e0d70b
SamuelRPi <n8135773@gmail.com>
2025-09-30 16:17:47 -0600

    barra de vida

--- funcionesdeteclado.gd
+++ funcionesdeteclado.gd
@@ -1,4 +1,9 @@
 extends VehicleBody3D
+@export var max_health: float = 100.0
+var current_health: float
+
+@export var health_bar: ProgressBar
+
 func _input(event):
 	if event.is_action_pressed("correr"):
 		print("correr occurred!")
@@ -8,13 +13,15 @@ func _input(event):
 		$llantaDI.engine_force = 80
 	if event.is_action_pressed("Reversa"):
 		print("Reversa occurred!")
-		$llantaTD.engine_force = -10
-		$llantaTI.engine_force = -10
+		$llantaTD.engine_force = -80
+		$llantaTI.engine_force = -80
+		$llantaDD.engine_force = -80
+		$llantaDI.engine_force = -80
 	if event.is_action_pressed("Izquierda"):
 		print("Izquierda occurred!")
-		$llantaDI.steering = deg_to_rad(-30)
+		$llantaDI.steering = deg_to_rad(30)
 	if event.is_action_pressed("Derecha"):
-		$llantaDD.steering = deg_to_rad(30)
+		$llantaDD.steering = deg_to_rad(-30)
 		print("Derecha occurred!")
 	if event.is_action_pressed("salto"):
 		print("salto occurred!")
@@ -24,4 +31,26 @@ func _input(event):
 		print("Reparar occurred!")
 	if event.is_action_pressed("Disparar"):
 		print("Disparar occurred!")
-	
+
+#Barra de vida
+signal died
+func _ready():
+	current_health = max_health
+	if health_bar:
+		health_bar.max_value = max_health
+		health_bar.value = current_health
+
+func take_damage(amount: float):
+	if current_health <= 0:
+		return
+
+	current_health -= amount
+	print("Vida restante: ", current_health)
+
+	if health_bar:
+		health_bar.value = current_health
+
+	if current_health <= 0:
+		emit_signal("died")
+		print("¡Vehículo destruido!")
+		queue_free()
