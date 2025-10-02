53fa45385e3dc56cc96d81a16932abfb40e6b5a8
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-10-02 13:28:54 -0600

    Corregí el manejo del auto
    Elimine la función de jhoan y añadi otra, ahora el auto se mueve con las 4 flechitas.

--- funcionesdeteclado.gd
+++ funcionesdeteclado.gd
@@ -2,45 +2,6 @@ extends VehicleBody3D
 @export var max_health: float = 100.0
 var current_health: float
 @export var health_bar: ProgressBar
-
-func _input(event):
-	if event.is_action_pressed("correr"):
-		print("correr occurred!")
-		$llantaTD.engine_force = 150
-		$llantaTI.engine_force = 150
-		$llantaDD.engine_force = 150
-		$llantaDI.engine_force = 150
-	elif event.is_action_pressed("Reversa"):
-		print("Reversa occurred!")
-		$llantaTD.engine_force = -150
-		$llantaTI.engine_force = -150
-		$llantaDD.engine_force = -150
-		$llantaDI.engine_force = -150
-	#else:
-		#$llantaTD.engine_force = 0
-		#$llantaTI.engine_force = 0
-		#$llantaDD.engine_force = 0
-		#$llantaDI.engine_force = 0
-	if event.is_action_pressed("Izquierda"):
-		print("Izquierda occurred!")
-		$llantaDI.steering = deg_to_rad(30)
-		$llantaDD.steering = deg_to_rad(30)
-	elif event.is_action_pressed("Derecha"):
-		$llantaDD.steering = deg_to_rad(-30)
-		$llantaDI.steering = deg_to_rad(-30)
-		print("Derecha occurred!")
-	#else:
-		#$llantaDD.steering = deg_to_rad(0)
-		#$llantaDI.steering = deg_to_rad(0)
-	if event.is_action_pressed("salto"):
-		print("salto occurred!")
-	if event.is_action_pressed("Remolcar"):
-		print("Remolcar occurred!")
-	if event.is_action_pressed("Reparar"):
-		print("Reparar occurred!")
-	if event.is_action_pressed("Disparar"):
-		print("Disparar occurred!")
-	
 #Barra de vida
 signal died
 func _ready():
@@ -48,6 +9,32 @@ func _ready():
 	if health_bar:
 		health_bar.max_value = max_health
 		health_bar.value = current_health
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("ui_left"):
+		$llantaDI.steering = deg_to_rad(30)
+		$llantaDD.steering = deg_to_rad(30)
+	elif Input.is_action_pressed("ui_right"):
+		$llantaDD.steering = deg_to_rad(-30)
+		$llantaDI.steering = deg_to_rad(-30)
+	else:
+		$llantaDD.steering = deg_to_rad(0)
+		$llantaDI.steering = deg_to_rad(0)
+	if Input.is_action_pressed("ui_up"):
+		$llantaTD.engine_force = 150
+		$llantaTI.engine_force = 150
+		$llantaDD.engine_force = 150
+		$llantaDI.engine_force = 150
+	elif Input.is_action_pressed("ui_down"):
+		$llantaTD.engine_force = -150
+		$llantaTI.engine_force = -150
+		$llantaDD.engine_force = -150
+		$llantaDI.engine_force = -150
+	else:
+		$llantaTD.engine_force = 0
+		$llantaTI.engine_force = 0
+		$llantaDD.engine_force = 0
+		$llantaDI.engine_force = 0
 
 func take_damage(amount: float):
 	if current_health <= 0:
