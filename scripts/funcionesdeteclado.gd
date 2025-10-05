8e7dc11654557ab832b326a737cd07442bc854cd
Oscar Alexis Dávila Reyes <104913466+Daro25@users.noreply.github.com>
2025-10-05 10:43:33 -0600

    Mapa con shaders
    Ya puse un cielo hermoso

---
+++ scripts/funcionesdeteclado.gd
@@ -0,0 +1,52 @@
+extends VehicleBody3D
+@export var max_health: float = 100.0
+var current_health: float
+@export var health_bar: ProgressBar
+#Barra de vida
+signal died
+func _ready():
+	current_health = max_health
+	if health_bar:
+		health_bar.max_value = max_health
+		health_bar.value = current_health
+
+func _physics_process(delta: float) -> void:
+	if Input.is_action_pressed("ui_left"):
+		$llantaDI.steering = deg_to_rad(30)
+		$llantaDD.steering = deg_to_rad(30)
+	elif Input.is_action_pressed("ui_right"):
+		$llantaDD.steering = deg_to_rad(-30)
+		$llantaDI.steering = deg_to_rad(-30)
+	else:
+		$llantaDD.steering = deg_to_rad(0)
+		$llantaDI.steering = deg_to_rad(0)
+	if Input.is_action_pressed("ui_up"):
+		$llantaTD.engine_force = 150
+		$llantaTI.engine_force = 150
+		$llantaDD.engine_force = 150
+		$llantaDI.engine_force = 150
+	elif Input.is_action_pressed("ui_down"):
+		$llantaTD.engine_force = -150
+		$llantaTI.engine_force = -150
+		$llantaDD.engine_force = -150
+		$llantaDI.engine_force = -150
+	else:
+		$llantaTD.engine_force = 0
+		$llantaTI.engine_force = 0
+		$llantaDD.engine_force = 0
+		$llantaDI.engine_force = 0
+
+func take_damage(amount: float):
+	if current_health <= 0:
+		return
+
+	current_health -= amount
+	print("Vida restante: ", current_health)
+
+	if health_bar:
+		health_bar.value = current_health
+
+	if current_health <= 0:
+		emit_signal("died")
+		print("¡Vehículo destruido!")
+		queue_free()
